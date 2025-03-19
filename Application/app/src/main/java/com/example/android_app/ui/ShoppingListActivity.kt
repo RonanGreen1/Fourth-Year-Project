4f1df0c89914a14ad36fa91b80eec1abc84ebb3d
RonanGreen1 <ronangreen2003@gmail.com>
2025-03-19 11:32:12 +0000

    Continued on shopping list

---
+++ Application/app/src/main/java/com/example/android_app/ui/ShoppingListActivity.kt
@@ -0,0 +1,12 @@
+package com.example.android_app.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.android_app.R
+
+class ShoppingListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shopping_list)
+    }
+}
